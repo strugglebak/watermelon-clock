62d7c5f104031973497929e070b99462c34a6507
strugglebak <strugglebak@gmail.com>
2019-12-18 22:36:53 +0800

    丰富了注册/登录页面

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import { Input, Button, Icon } from 'antd'
 
 export class login extends Component {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      account: '',
+      password: '',
+    }
+  }
+
+  onAccountInputChange = (e: any) => {
+    const account = e.target.value
+    this.setState({ account })
+  }
+  onPasswordInputChange = (e: any) => {
+    const password = e.target.value
+    this.setState({ password })
+  }
+
   render() {
     return (
-      <div>
-        登陆页
-      </div>
+      <>
+        <h1>登录</h1>
+        <Input
+          placeholder="请输入账号" allowClear
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          onChange={this.onAccountInputChange}
+        />
+        <Input.Password placeholder="请输入密码" allowClear
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          onChange={this.onPasswordInputChange}
+        />
+        <Button type="primary">登录</Button>
+      </>
     )
   }
 }
--- src/components/signUp/signUp.tsx
+++ src/components/signUp/signUp.tsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
+import { Input, Button, Icon } from 'antd'
 
 export class signUp extends Component {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      account: '',
+      password: '',
+      passwordConfirmed: ''
+    }
+  }
+
+
+  onAccountInputChange = (e: any) => {
+    const account = e.target.value
+    this.setState({ account })
+  }
+  onPasswordInputChange = (e: any) => {
+    const password = e.target.value
+    this.setState({ password })
+  }
+  onPasswordConfirmedInputChange = (e: any) => {
+    const passwordConfirmed = e.target.value
+    this.setState({ passwordConfirmed })
+  }
+
   render() {
     return (
-      <div>
-        注册页
-      </div>
+      <>
+        <h1>注册</h1>
+        <Input
+          placeholder="请输入账号" allowClear
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          onChange={this.onAccountInputChange}
+        />
+        <Input.Password placeholder="请输入密码" allowClear
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          onChange={this.onPasswordInputChange}
+        />
+        <Input.Password placeholder="确认密码" allowClear
+          prefix={<Icon type="safety-certificate" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          onChange={this.onPasswordConfirmedInputChange}
+        />
+        <Button type="primary">注册</Button>
+      </>
     )
   }
 }
