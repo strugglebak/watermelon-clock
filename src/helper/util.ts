3d2c3728f198b6dfa5ba2406cd3132d22653238a
moss <dxp157@sohu.com>
2020-01-11 22:28:15 +0800

    增加时间转换的公共函数

--- src/helper/util.ts
+++ src/helper/util.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 /**
  * 
  * @param element 监听元素
@@ -11,7 +13,7 @@ export function delegate(
   eventType: any, 
   selector: any, 
   fn: any
-) {
+): any {
     element.addEventListener(eventType, (e: any) => {
       let el = e.target
       while (!el.matches(selector)) {
@@ -25,3 +27,24 @@ export function delegate(
     })
     return element
    }
+
+export function dayOfWeekTransfer(date: string): any {
+  const dayMap = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+  const index = parseInt(format(new Date(date), 'i'), 10) - 1
+  return dayMap[index]
+}
+
+export function yearMonthDayTransfer(date: string): any {
+  const arr = date.split('-')
+  return `${arr[0]}年${arr[1]}月${arr[2]}日`
+}
+
+
+export function timeTransfer (time: string, formatText: string, itemType: string): any {
+    const formatedTime = format(new Date(time), formatText)
+    const arr = formatedTime.split('-')
+    const str = itemType === 'finished'
+      ? formatedTime
+      : `${arr[0]}月${arr[1]}日`
+    return str
+  }
