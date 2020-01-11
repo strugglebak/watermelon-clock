a9f5470b53e5228a682a1c40fea6586c563aa539
moss <dxp157@sohu.com>
2020-01-11 17:09:30 +0800

    点击 statistics 对应 tab , 可以展开对应的数据

--- src/components/statistics/statistics.tsx
+++ src/components/statistics/statistics.tsx
@@ -4,6 +4,10 @@ import { format } from 'date-fns'
 import _ from 'lodash'
 import Polyline from './polyline'
 import TodosHistory from './todosHistory/todosHistory'
+import WaterMelonsHistory from './waterMelonsHistory/waterMelonsHistory'
+import SummaryHistory from './summaryHistory/summaryHistroy'
+import { delegate } from '../../helper/util'
+import classNames from 'classnames'
 
 import './statistics.styl'
 
@@ -11,8 +15,43 @@ interface IStatisticsProps {
   todos: any[]
 }
 
+interface IStatisticsState {
+  mapTitleVisible: any
+}
+
 export class statistics extends Component
-<IStatisticsProps, any> {
+<IStatisticsProps, IStatisticsState> {
+
+  constructor(props: IStatisticsProps) {
+    super(props)
+    this.state = {
+      mapTitleVisible: {
+       summaryTitle: { visible: false },
+       watermelonTitle: { visible: false },
+       todosTitle: { visible: false }
+      }
+    }
+  }
+  
+  componentDidMount() {
+    delegate(this.ulRef.current, 'click', 'li', (e: any, el: any) => {
+      const { className } = el
+      this.setState({
+        mapTitleVisible: {...this.initMapVisible(className)}
+      })
+    })
+  }
+
+  initMapVisible = (className: string) => {
+    const newClassName = className.split(' ')[0]
+    const { mapTitleVisible } = this.state
+    const { visible } = mapTitleVisible[newClassName]
+    mapTitleVisible[newClassName].visible = !visible
+    Object.keys(mapTitleVisible).map(key => {
+      !newClassName.includes(key) && (mapTitleVisible[key].visible = false)
+    })
+    return mapTitleVisible
+  }
 
   get finishedTodos() { 
     return this.props.todos.filter((todo: any) => todo.completed && !todo.deleted) 
@@ -25,19 +64,36 @@ export class statistics extends Component
     })
   }
 
+  ulRef = React.createRef<HTMLUListElement>()
+
   render() {
+    const summaryTitleClasses = classNames({
+      summaryTitle: true,
+      visible: this.state.mapTitleVisible.summaryTitle.visible
+    })
+
+    const watermelonTitleClasses = classNames({
+      watermelonTitle: true,
+      visible: this.state.mapTitleVisible.watermelonTitle.visible
+    })
+
+    const todosTitleClasses = classNames({
+      todosTitle: true,
+      visible: this.state.mapTitleVisible.todosTitle.visible
+    })
+
     return (
       <>
-      <ul className="statistics">
-        <li>
+      <ul className="statistics" ref={this.ulRef}>
+        <li className={summaryTitleClasses}>
           <h3 className="title">统计</h3>
           <p className="description">一周统计</p>
         </li>
-        <li>
+        <li className={watermelonTitleClasses}>
           <h3 className="title">西瓜历史</h3>
           <p className="description">累计完成西瓜</p>
         </li>
-        <li>
+        <li className={todosTitleClasses}>
           <h3 className="title">任务历史</h3>
           <p className="description">累计完成任务 {this.finishedTodos.length} 个</p>
           <Polyline
@@ -46,7 +102,17 @@ export class statistics extends Component
           />
         </li>
       </ul>
-      <TodosHistory />
+      <div className="history-wrapper">
+        <div className={summaryTitleClasses}>
+          <SummaryHistory/>
+        </div>
+        <div className={watermelonTitleClasses}>
+          <WaterMelonsHistory/>
+        </div>
+        <div className={todosTitleClasses}>
+          <TodosHistory/>
+        </div>
+      </div>
       </>
     )
   }
