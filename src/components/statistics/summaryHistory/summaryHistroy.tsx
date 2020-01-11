6a4d088bd8065dd750acb7bacb4d1ba796dd6bd2
moss <dxp157@sohu.com>
2020-01-11 17:08:46 +0800

    增加一周统计以及西瓜历史组件

---
+++ src/components/statistics/summaryHistory/summaryHistroy.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './summaryHistory.styl'
+
+export class summaryHistory extends Component {
+  render() {
+    return (
+      <div className="week-data-history">
+        这里是 summaryHistory
+      </div>
+    )
+  }
+}
+
+export default summaryHistory
---
+++ src/components/statistics/waterMelonsHistory/waterMelonsHistory.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './waterMelonsHistory.styl'
+
+export class waterMelonsHistory extends Component {
+  render() {
+    return (
+      <div className="watermelons-history">
+        这里是 waterMelonsHistory
+      </div>
+    )
+  }
+}
+
+export default waterMelonsHistory
