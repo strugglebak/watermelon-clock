a774d3a1237a00e4cf4317fb41872db853bc2cc2
strugglebak <strugglebak@gmail.com>
2020-03-06 11:40:57 +0800

    修复了当只有一天完成多项任务时，折线图绘制不能绘制出来的 bug

--- src/components/statistics/polyline.tsx
+++ src/components/statistics/polyline.tsx
@@ -12,13 +12,13 @@ export class polyline extends Component
 <IPolylineProps, any> {
 
   points = () => {
-    const datesKeys = Object.keys(this.props.data).sort(
+    let { data, finishedCount, width } = this.props
+    const datesKeys = Object.keys(data).sort(
       (a, b) => {
         return Date.parse(a) - Date.parse(b)
       }
     )
 
-    let { width } = this.props
 
     const firstDay = datesKeys[0]
     const startPoints = '0,60'
@@ -26,7 +26,8 @@ export class polyline extends Component
     if (!firstDay) return [`${startPoints}`, `${lastPoints}`].join(' ')
 
     const lastDay = datesKeys[datesKeys.length - 1]
-    const dayRange = Date.parse(lastDay) - Date.parse(firstDay)
+    // 若是同一天有很多任务，并且只有这一天的情况下，dayRange 有可能是 0
+    const dayRange = Date.parse(lastDay) - Date.parse(firstDay) || 1
     let count = 0
     let lastY
     // 画折线图需要 x y 坐标
@@ -35,7 +36,7 @@ export class polyline extends Component
     const pointsArray = datesKeys.map((datesKey: any) => {
       const x = (Date.parse(datesKey) - Date.parse(firstDay)) / dayRange * (width || 320)
       count += this.props.data[datesKey].length
-      const y = (1 -  (count / this.props.finishedCount)) * 60
+      const y = (1 -  (count / finishedCount)) * 60
       lastY = y
       return `${x},${y}`
     })
@@ -45,10 +46,10 @@ export class polyline extends Component
   render() {
     return (
       <svg className="polyline peity" width="100%" height="60">
-        <polygon 
-          fill="rgba(215,78,78,0.1)" 
-          stroke="rgba(215,78,78,0.5)" 
-          strokeWidth="1" 
+        <polygon
+          fill="rgba(215,78,78,0.1)"
+          stroke="rgba(215,78,78,0.5)"
+          strokeWidth="1"
           points={this.points()}
         />
       </svg>
