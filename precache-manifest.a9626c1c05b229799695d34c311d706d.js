5bb91db6b0821a06a89993b3b0a2f9371f89969e
strugglebak <strugglebak@gmail.com>
2020-03-06 17:19:37 +0800

    Updates

---
+++ precache-manifest.a9626c1c05b229799695d34c311d706d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802f7da3c858066084ee44e2e1c354d7",
+    "url": "/watermelon-clock/index.html"
+  },
+  {
+    "revision": "00b778eed4424ea7a737",
+    "url": "/watermelon-clock/static/css/2.9dd65991.chunk.css"
+  },
+  {
+    "revision": "966eb34e129a2c060b14",
+    "url": "/watermelon-clock/static/css/main.8455e325.chunk.css"
+  },
+  {
+    "revision": "00b778eed4424ea7a737",
+    "url": "/watermelon-clock/static/js/2.c871527e.chunk.js"
+  },
+  {
+    "revision": "a2aa620a573a215f189a96258e576add",
+    "url": "/watermelon-clock/static/js/2.c871527e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "966eb34e129a2c060b14",
+    "url": "/watermelon-clock/static/js/main.ceae2608.chunk.js"
+  },
+  {
+    "revision": "56d3f463503c77baba5b",
+    "url": "/watermelon-clock/static/js/runtime-main.551e29e2.js"
+  },
+  {
+    "revision": "6c4bd1c993a6dfa42beb272fc68f1a86",
+    "url": "/watermelon-clock/static/media/logo.6c4bd1c9.png"
+  }
+]);
